b92f05464b7a8f86b6a9a2dc59e9e01147e219cd
Kurt Lawrence Dela Cruz <147698323+Dokkone@users.noreply.github.com>
2024-07-22 23:06:28 +0800

    Update LibraryCard.sol

--- ETH + AVAX Intermediate/Smart Contract Management/contracts/LibraryCard.sol
+++ ETH + AVAX Intermediate/Smart Contract Management/contracts/LibraryCard.sol
@@ -5,23 +5,30 @@ contract LibraryCard {
     address payable public owner;
     mapping(string => bool) public registeredBooks;
     mapping(string => bool) public borrowedBooks;
+    uint public registrationFee;
+    mapping(address => bool) public hasPaid;
 
     event BookRegistered(string book);
     event BookBorrowed(string book);
     event BookReturned(string book);
 
-    constructor() {
+    constructor(uint _registrationFee) {
         owner = payable(msg.sender);
+        registrationFee = _registrationFee;
+    }
+
+    function payRegistrationFee() public payable {
+        require(msg.value == registrationFee, "Incorrect registration fee");
+        hasPaid[msg.sender] = true;
     }
 
     function registerBook(string memory _book) public {
-        require(msg.sender == owner, "You are not the owner of this account");
+        require(hasPaid[msg.sender], "You have not paid the registration fee");
         registeredBooks[_book] = true;
         emit BookRegistered(_book);
     }
 
     function borrowBook(string memory _book) public {
-        require(msg.sender == owner, "You are not the owner of this account");
         require(registeredBooks[_book], "This book is not registered");
         require(!borrowedBooks[_book], "This book is already borrowed");
         
@@ -30,10 +37,14 @@ contract LibraryCard {
     }
 
     function returnBook(string memory _book) public {
-        require(msg.sender == owner, "You are not the owner of this account");
         require(borrowedBooks[_book], "This book is not borrowed");
         
         borrowedBooks[_book] = false;
         emit BookReturned(_book);
+    }
+
+    function withdraw() public {
+        require(msg.sender == owner, "You are not the owner");
+        owner.transfer(address(this).balance);
     }
 }
